c5bcf1832dad078cd634d0537b83e339f6764403
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-05-28 23:24:34 -0300

    feat: definição das rotas resource para Contato

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContatoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContatoController::class, 'index'])->name('index');
+Route::resource('contatos', ContatoController::class)->except(['index']);
